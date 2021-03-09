b5a3b5019d2d30a18eab5f62888f039623c86097
dave oh <hirakoshinji722@gmail.com>
2021-03-09 06:44:27 +0100

    setup: initial commit

---
+++ buildSrc/src/main/kotlin/io/davidosemwota/template/buildsource/BuildPlugins.kt
@@ -0,0 +1,9 @@
+@file:Suppress("SpellCheckingInspection")
+package io.davidosemwota.template.buildsource
+
+object BuildPlugins {
+
+    const val spotless = "com.diffplug.spotless"
+    const val android_application = "com.android.application"
+    const val kotlin_kapt = "kotlin-kapt"
+}
